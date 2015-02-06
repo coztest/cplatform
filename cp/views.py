74da2e55750190d75ca36f972350a0f1c1b8020f
Lyon <fengxueyu@126.com>
2015-02-06 15:06:46 +0800

    complete manage project function and fix some bugs

--- cp/views.py
+++ cp/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,6 +7,8 @@ from django.template import RequestContext
 import gevent.subprocess
 import gevent
 import time
+import os
+import signal
 from models import *
 from gevent.local import local
 from dwebsocket.decorators import accept_websocket
@@ -18,14 +21,25 @@ sys.setdefaultencoding('utf-8')
 def index(request):
     return render_to_response("login.html",context_instance=RequestContext(request))
 def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    if username == "123" and password == "1":
-        request.session["username"] = 123
+    login_username = request.POST['username']
+    login_password = request.POST['password']
 
-        return HttpResponseRedirect("/mainpage")
+
+    try:
+        user = User.objects.get(username=login_username)
+    except User.DoesNotExist:
+            return HttpResponse("user is not exist, please register!")
     else:
-        return HttpResponse("username or password is not correct!")
+        if login_password == user.password:
+            request.session["username"] = user.username
+
+            return HttpResponseRedirect("/mainpage")
+        else:
+            return HttpResponse("用户名或密码不正确!")
+
+
+
+
 
 
 def newproj(request):
@@ -43,34 +57,44 @@ def newproj(request):
 
 def mainpage(request):
     n = request.session["username"]
-
+    current_user = User.objects.get(username=n)
  #   i1_avai = request.session["instance1_available"]
     i1_avai = Instance.objects.get(id=1).available
-    proj_list = Project.objects.all()
+
+
+    proj_list = Project.objects.filter(user=current_user)
 
 
     return render_to_response("home.html",{"username":n,"proj_list":proj_list,"i1_avai":i1_avai})
 
-
-def instance1(request):
-    return render_to_response("instance1.html")
-
 def upload1(request,proj_id):
+
+    popen_clear_ls = gevent.subprocess.Popen(['ls /home/li/abc/'], stdout = gevent.subprocess.PIPE,shell=True)
+    popen_clear_ls_out = popen_clear_ls.stdout.readlines()
+    if popen_clear_ls_out != []:
+          popen_clear_rm = gevent.subprocess.call('rm -r /home/li/abc/*', shell=True)
+
     file_obj = request.FILES.get('upload')
+
     filename = file_obj.name
-    print(filename)
+    print "***"
+    print file_obj
+    print "***"
+
     f = open("/home/li/abc/"+filename,"wb+")
     for chunk in  file_obj.chunks():
         f.write(chunk)
     f.close()
     popen_tar = gevent.subprocess.Popen(['tar','-zxf',"/home/li/abc/"+filename,"-C","/home/li/abc/"])
+    popen_tar.wait()
     popen2_rm = gevent.subprocess.Popen(['rm','/home/li/abc/'+filename])
     popen3_ls = gevent.subprocess.Popen(['ls /home/li/abc/'], stdout = gevent.subprocess.PIPE,shell=True)
     popen3_ls_out = popen3_ls.stdout.readlines()
+    print popen3_ls_out
     proj_sourcelist = ""
     i = 1
     for source_name in popen3_ls_out:
-        if i != len(popen3_ls_out):
+        if i < len(popen3_ls_out):
             proj_sourcelist += source_name.replace("\n",", ")
         else:
             proj_sourcelist += source_name.replace("\n","")
@@ -84,26 +108,43 @@ def upload1(request,proj_id):
 
 @accept_websocket
 def echo(request):
+
     if request.is_websocket:
-        print "gogogo1"
-        print "gogogo2"
         for ms in request.websocket:
-            if ms == "go":
-                print "gogogo3"
-                g = gevent.spawn(runterm1,request)
+            if ms.split('-')[0] == "go":
+                proj_id = ms.split('-')[1]
+                g = gevent.spawn(runterm1,request,proj_id)
                 Instance.objects.filter(id=1).update(available=False)
                 g.start()
 
-            elif ms == "stop":
-                print "gogogo4"
-                g.kill()
+            elif ms.split('-')[0] == "stop":
+                proj_id = ms.split('-')[1]
                 term1_pid = request.session.get("term1_pid")
                 print "kill "+ term1_pid
-                popen = gevent.subprocess.call("kill "+term1_pid,shell=True)
+
+                os.kill(term1_pid,signal.SIGINT)
+                g.kill()
+
+                popen_cat = gevent.subprocess.Popen(['cat /home/li/compile_env/workers/instance1/state/pkgs_done'], stdout = gevent.subprocess.PIPE,shell=True)
+                popen_cat_out = popen_cat.stdout.readlines()
+                proj_done_sourcelist = ""
+                j = 1
+                for source_name in popen_cat_out:
+                    if j < len(popen_cat_out):
+                        proj_done_sourcelist += source_name.replace("\n",", ")
+                    else:
+                        proj_done_sourcelist += source_name.replace("\n","")
+                    j += 1
+                del j
+                Project.objects.filter(id=proj_id).update(complete_proj=proj_done_sourcelist)
+
+
+
+
 		Instance.objects.filter(id=1).update(available=True)
 
 
-def runterm1(request):
+def runterm1(request,proj_id):
 
     popen = gevent.subprocess.Popen(['bash','/home/li/run_instance1.sh'], stdout = gevent.subprocess.PIPE)
 #    popen = gevent.subprocess.Popen(['ping','127.0.0.1'], stdout = gevent.subprocess.PIPE)
@@ -116,6 +157,19 @@ def runterm1(request):
         gevent.sleep(0.001)
     Instance.objects.filter(id=1).update(available=True)
 
+
+    popen_cat = gevent.subprocess.Popen(['cat /home/li/compile_env/workers/instance1/state/pkgs_done'], stdout = gevent.subprocess.PIPE,shell=True)
+    popen_cat_out = popen_cat.stdout.readlines()
+    proj_done_sourcelist = ""
+    j = 1
+    for source_name in popen_cat_out:
+        if j < len(popen_cat_out):
+            proj_done_sourcelist += source_name.replace("\n",", ")
+        else:
+            proj_done_sourcelist += source_name.replace("\n","")
+        j += 1
+    del j
+    Project.objects.filter(id=proj_id).update(complete_proj=proj_done_sourcelist)
 
 
 
